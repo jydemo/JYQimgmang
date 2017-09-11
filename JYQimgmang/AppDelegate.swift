d1b88877205e9eb7a9e65be2e7fec817c34fbcf2
jydemo <jydemo@qq.com>
2017-09-11 14:11:01 +0800

    Initial Commit

---
+++ JYQimgmang/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  JYQimgmang
+//
+//  Created by JYKit on 2017/9/11.
+//  Copyright © 2017年 JYKit. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
---
+++ JYQimgmang/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  JYQimgmang
+//
+//  Created by JYKit on 2017/9/11.
+//  Copyright © 2017年 JYKit. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
